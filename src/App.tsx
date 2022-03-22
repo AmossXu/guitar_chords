2b99bfec2bb2052da2ce4a031ac8f7647cb68073
Amos <mots.xu@gmail.com>
2022-03-22 22:24:57 +0800

    feat: findKeyIndex & step function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Tone from './utils/chords/Tone'
@@ -7,6 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
   const tone = new Tone('1')
 
+  useEffect(() => {
+    console.log(tone);
+    console.log(tone.step(6));
+    console.log(tone.step(5));
+
+  }, [])
   return (
     <div>1</div>
   )
--- src/utils/chords/Tone/index.ts
+++ src/utils/chords/Tone/index.ts
@@ -1,3 +1,4 @@
+import { is } from "../../type"
 import { GuitarPosition, IntervalItem, SyllableItem, ToneString, ToneStringItem } from "./definition"
 
 class Tone {
@@ -12,7 +13,7 @@ class Tone {
     private sharp: '#' | ''
     private octave: number
     private position: GuitarPosition
-    constructor(toneStr: ToneStringItem = ToneString.DO, string: number, fret: number) {
+    constructor(toneStr: ToneStringItem = ToneString.DO, string?: number, fret?: number) {
         this.toneString = toneStr
         this.syllableMap = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si']; // 所有唱名数组
         this.keyMap = ['1', ['#1', 'b2'], '2', ['#2', 'b3'], '3', '4', ['#4', 'b5'], '5', ['#5', 'b6'], '6', ['#6', 'b7'], '7']; // 音程
@@ -29,9 +30,57 @@ class Tone {
         this.octave = (octave_sharp ? octave_sharp.length : 0) - (octave_flat ? octave_flat.length : 0); // 八度度数
         // 吉他按弦位置
         this.position = {
-            string: string, // 第几弦
-            fret: fret // 第几品格
+            string: string ?? 0, // 第几弦
+            fret: fret ?? 0// 第几品格
         };
+    }
+    // 获取某个音在音程上的位置
+    findKeyIndex(keyString: ToneStringItem) {
+        return this.keyMap.findIndex((item) => {
+            if (is(item)('Array')) {
+                return item.includes(keyString);
+            } else if (item === keyString) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+    // 音高增减，num为增或减的半音数量
+    step(num: number) {
+        let keyString = this.flat + this.sharp + this.key;
+        let len = this.keyMap.length;
+        let index = this.findKeyIndex(keyString as ToneStringItem) as number;
+        if (index > -1) {
+            num = +num;
+            // 计算改变音高后的音在音程上的位置
+            let nextIndex = parseInt(String(index) + num, 0);
+            let octave = this.octave;
+            if (nextIndex >= len) {
+                let index_gap = nextIndex - len;
+                octave += Math.floor(index_gap / len) + 1;
+                nextIndex = index_gap % len;
+            } else if (nextIndex < 0) {
+                let index_gap = nextIndex;
+                octave += Math.floor(index_gap / len);
+                nextIndex = index_gap % len + len;
+            }
+            let nextKey = this.keyMap[nextIndex];
+            // 计算并添加高低八度的记号
+            let octaveString = new Array(Math.abs(octave)).fill('.').join('');
+            let toneString = '';
+            if (!is(nextKey)('Array')) {
+                toneString = (octave < 0 ? octaveString : '') + nextKey + (octave > 0 ? octaveString : '');
+                return new Tone(toneString as ToneStringItem, this.position.string, this.position.fret + num);
+            } else {
+                // 可能得到两个音高一样但标记方式不一样的音
+                return (nextKey as ToneStringItem[]).map((key) => {
+                    return new Tone((octave < 0 ? octaveString : '') + key + (octave > 0 ? octaveString : '') as ToneStringItem, this.position.string, this.position.fret + num);
+                });
+            }
+        } else {
+            return null;
+        }
     }
 }
 
